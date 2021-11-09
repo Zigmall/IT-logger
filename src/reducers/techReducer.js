9a2b90899b1938c80651abf80303b992b5f6abd9
Zigmal <rafal.hes@yahoo.com>
2021-11-09 20:54:38 +0100

    Add getTechs to redux

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,3 +1,11 @@
+import {
+    GET_TECHS,
+    ADD_TECH,
+    DELETE_TECH,
+    SET_LOADING,
+    TECHS_ERROR,
+  } from '../actions/types';
+ 
  const initialState = {
      techs: null,
      loading: false,
@@ -7,7 +15,17 @@
  // eslint-disable-next-line
  export default (state = initialState, action) => {
     switch(action.type) {
-        
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+            case GET_TECHS:
+                return {
+                    ...state,
+                    techs: action.payload,
+                    loading: false
+                }
         default:
             return state;
     }
