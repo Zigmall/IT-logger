40156ce896fd43071ef8ac0ae095b2345ff8311e
Zigmal <rafal.hes@yahoo.com>
2021-11-10 12:03:16 +0100

    Add delete technitian functionality

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteTech } from '../../actions/techAction';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const TechItem = ({ tech }) => {
+
+
+const TechItem = ({ tech, deleteTech}) => {
+  const onDelete = () => {
+    deleteTech(tech.id);
+    M.toast({ html: 'Please enter a message and tech' });
+  };
+
   return (
     <li className='collection-item'>
       <div>
         {tech.firstName} {tech.lastName}
-        <a href='#!' className='secondary-content'>
+        <a href='#!' className='secondary-content' onClick={onDelete} >
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -16,6 +26,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
   tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.object.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
